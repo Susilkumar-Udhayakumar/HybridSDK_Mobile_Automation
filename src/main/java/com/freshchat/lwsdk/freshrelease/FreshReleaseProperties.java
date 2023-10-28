23bc8b14049caafa8e621fbc14fc80825797bec2
Susilkumar-Udhayakumar <susilkumar.udhayakumar@freshworks.com>
2023-10-28 13:44:52 +0530

    review comment update

--- src/main/java/com/freshchat/lwsdk/freshrelease/FreshReleaseProperties.java
+++ src/main/java/com/freshchat/lwsdk/freshrelease/FreshReleaseProperties.java
@@ -1,4 +1,4 @@
-package lwsdk.freshrelease;
+package com.freshchat.lwsdk.freshrelease;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -7,7 +7,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
-import lwsdk.app.logger.Log;
+import com.freshchat.lwsdk.app.logger.Log;
 
 
 public class FreshReleaseProperties {
